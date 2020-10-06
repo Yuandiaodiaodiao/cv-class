c9d06d816234064a380ac8d1bbb05bc472a10808
Yuandiaodiaodiao <qq295087430@outlook.com>
2020-10-06 20:59:52 +0800

    实验1实验2

---
+++ lightTransform.py
@@ -0,0 +1,53 @@
+import cv2
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+img=cv2.imread("test.jpg")
+img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+def doGamma(img,gamma):
+    gamma_img1 = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            for k in range(img.shape[2]):
+                gamma_img1[i, j, k] = int(math.floor(min(1,math.pow(img[i, j, k]/255, gamma))*255))
+    return gamma_img1
+plt.subplot(231)
+gamma1=doGamma(img,0.5)
+plt.imshow(gamma1)
+plt.subplot(232)
+gamma2=doGamma(img,1)
+plt.imshow(gamma2)
+plt.subplot(233)
+gamma3=doGamma(img,2)
+plt.imshow(gamma3)
+# plt.show()
+
+# img=cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+# cv2.imshow("0",img)
+# cv2.waitKey()
+shape=img.shape
+print(shape)
+# img=img/255
+def dolog(img,logArg):
+    c=logArg
+    logimage = np.zeros(img.shape, dtype=np.uint8)
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            for k in range(img.shape[2]):
+                col=c*(math.log(1.0+img[i,j,k]/255.0,2))
+            # print(c*math.log(255+255,500))
+            # if col>1:col=1
+                logimage[i][j][k] = math.floor(min(1,col)*255)
+    return logimage
+# img=np.where(255<=img,255,img)
+logimg1=dolog(img,0.5)
+plt.subplot(234)
+plt.imshow(logimg1)
+logimg2=dolog(img,1)
+plt.subplot(235)
+plt.imshow(logimg2)
+logimg3=dolog(img,2)
+plt.subplot(236)
+plt.imshow(logimg3)
+plt.show()
+
---
+++ sizeTransform.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+def resize(img,scalex,scaley):
+    imgy,imgx,_=img.shape
+    newx=math.ceil(scalex*imgx)
+    newy=math.ceil(scaley*imgy)
+    def getTargetPos(y,x):
+        return math.floor(y/scaley),math.floor(x/scalex)
+    newimage = np.zeros((newy,newx,img.shape[2]), dtype=np.uint8)
+    def transform(img,x1,x2,y1,y2,x,y):
+        print((x1,x2,y1,y2,x,y))
+        x=(x-x1)/(x2-x1)
+        y=(y-y1)/(y2-y1)
+        newcol=(img[y1][x1]*(1-x)*(1-y)
+                +img[y1][x2]*x*(1-y)
+                +img[y2][x1]*(1-x)*y
+                +img[y2][x2]*x*y)
+        return newcol
+    for i in range(newy):
+        for j in range(newx):
+            y,x=getTargetPos(i,j)
+            x1=math.floor(x)
+            x2=min(math.ceil(x+0.001),imgx-1)
+            y1=math.floor(y)
+            y2=min(math.ceil(y+0.001),imgy-1)
+            newimage[i][j]=transform(img,x1,x2,y1,y2,x,y)
+    return newimage
+if __name__=="__main__":
+
+    img = cv2.imread("test.jpg")
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    plt.subplot(131)
+    plt.imshow(img)
+    newimg=resize(img,0.5,0.5)
+    plt.subplot(132)
+    plt.imshow(newimg)
+    newimg = resize(img, 0.1, 0.1)
+    plt.subplot(133)
+    plt.imshow(newimg)
+    plt.show()
