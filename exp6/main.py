cf2ca2159a1b1043df74096357692ddc657ff642
苑 <33371662+Yuandiaodiaodiao@users.noreply.github.com>
2020-11-24 09:55:18 +0800

    玄学优化 参数微调分层不同训练速度

--- exp6/main.py
+++ exp6/main.py
@@ -11,13 +11,16 @@ def timeShow(fn):
 from torchvision import datasets, transforms
 
 imageTransform = transforms.Compose([
-    # transforms.RandomCrop(32, padding=4),
-    # transforms.RandomHorizontalFlip(),
-    # transforms.Resize(224),                  #resnet默认图片输入大小224*224
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
-    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+    transforms.Normalize((.5,.5,.5),(.5,.5,.5))
 ])
+testTransform = transforms.Compose([
 
+    transforms.ToTensor(),
+    transforms.Normalize((.5,.5,.5),(.5,.5,.5))
+])
 import torch
 
 #玄学加速
@@ -105,8 +108,27 @@ def test():
 if __name__ == "__main__":
 
     #带预训练的初始模型
-    modelNetTorch = torchvision.models.densenet169(pretrained=False,num_classes=10)
+    modelNetTorch = torchvision.models.densenet169(pretrained=True)
+    modelNetTorch.classifier=nn.Linear(modelNetTorch.classifier.in_features, 10)
+    # for k, v in modelNetTorch.named_parameters():
+    #     # v.requires_grad = True
+    #     # k 是某一层模型名称，v为该层的模型参数
+    #     v.requires_grad = False
+    #     if "classifier" in k :
+    #         v.requires_grad = True
+    #     print(k, v.requires_grad)
+    from vit_pytorch import ViT
 
+    modelNetVit = ViT(
+        image_size=32,
+        patch_size=4,
+        num_classes=10,
+        dim=768,
+        depth=12,
+        heads=12,
+        mlp_dim=3072,
+        dropout=0.1,
+    )
     #自己添油加醋
     class Net(nn.Module):
         def __init__(self,net):
@@ -122,37 +144,52 @@ if __name__ == "__main__":
     modelNet=Net(modelNetTorch)
     modelNet=modelNetTorch
     #存档名字
-    NetTitle="wide_resnet101_2"
+    NetTitle="densenet169"
     #爆显存了就缩一缩
-    train_batch_size=512
-    test_batch_size=512
+    train_batch_size=1024
+    test_batch_size=1024
+    lr=0.1
     testrate=0
 
     trainDataset = datasets.CIFAR10('./train', train=True, download=True, transform=imageTransform)
-    train_loader = torch.utils.data.DataLoader(trainDataset, batch_size=train_batch_size, pin_memory=True,
+    train_loader = torch.utils.data.DataLoader(trainDataset, batch_size=train_batch_size, pin_memory=True,shuffle=True,
                                                num_workers=0)
 
-    testDataset = datasets.CIFAR10('./train', train=False, download=True, transform=imageTransform)
+    testDataset = datasets.CIFAR10('./train', train=False, download=True, transform=testTransform)
 
     test_loader = torch.utils.data.DataLoader(testDataset, batch_size=test_batch_size)
 
-    testTrainDataset = datasets.CIFAR10('./train', train=True, download=True, transform=imageTransform)
+    testTrainDataset = datasets.CIFAR10('./train', train=True, download=True, transform=testTransform)
     test_train_loader = torch.utils.data.DataLoader(testTrainDataset, batch_size=test_batch_size)
+    def changegrad(net):
+        ignored_params = list(map(id, net.classifier.parameters()))
+        base_params = filter(lambda p: id(p) not in ignored_params, net.parameters())
+        # 对不同参数设置不同的学习率
+        params_list = [{'params': base_params, 'lr': 0.001}, ]
+        params_list.append({'params': net.classifier.parameters(), 'lr': 0.01})
+        for k, v in net.named_parameters():
+            v.requires_grad = True
+        return params_list
     try:
 
         save = torch.load(f'./model/{NetTitle}Newest')
         model = save["model"]
+        params_list=changegrad(model)
         model.cuda()
-        optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=0.05, momentum=0.9,
-                                    weight_decay=5e-4)
-        optimizer.load_state_dict(save["optimizer"])
+        optimizer = torch.optim.SGD(params_list, lr=lr, momentum=0.9,
+                                    weight_decay=0.00008)
+        try:
+            optimizer.load_state_dict(save["optimizer"])
+        except:
+            pass
         epoch = save["epoch"]
         print(f"load from './model/{NetTitle}Newest'")
     except:
         # 全新训练
+        params_list=changegrad(modelNet)
         model = modelNet.cuda()
-        optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=0.05, momentum=0.9,
-                                    weight_decay=5e-4)
+        optimizer = torch.optim.SGD(params_list, lr=lr, momentum=0.9,
+                                    weight_decay=0.00008)
         epoch = 0
 
 
